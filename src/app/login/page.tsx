dcc0cf811ef5f48b1eacb2a828a9a48d98706394
Matt Mocklin <matt.mocklin@gmail.com>
2024-11-11 14:42:22 -0500

    Added image to left side of home page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,9 @@
 export default function Login() {
     return(
      <div>
+        <form>
+            
+        </form>
       <h1>LOGIN PAGE</h1>
      </div>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,20 @@ import Header from "./components/Header"
 
 export default function Home() {
   return (
-    <div>
-      <nav>
+    <div className="grid grid-cols-3 h-screen">
+      <div className="col-span-1">
+       <Image
+        src="/homeImage.jpg"
+        alt="bookcases"
+        width={1000}
+        height={1000}
+        className="w-full h-full object-cover"
+       />
+      </div>
+      <div className="col-span-2">
        <Header></Header>
-      </nav>
+       <h1 className="text-3xl font-bold text-center">Welcome to Librium! Login or Signup to get Started!</h1>
+      </div>
     </div>
   );
 }
