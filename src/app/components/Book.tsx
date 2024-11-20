90e6fcc72fc51a02af289216f4fcbbe6814c781e
Matt Mocklin <matt.mocklin@gmail.com>
2024-11-20 18:06:05 -0500

    minor book styling update

--- src/app/components/Book.tsx
+++ src/app/components/Book.tsx
@@ -15,16 +15,18 @@ type BookProps = {
 export default function Book({ book, onDelete, onUpdate}: BookProps) {
     return (
       <div className="relative shadow rounded-xl bg-blue-500 p-3 flex flex-col items-center flex-wrap w-[200px] border border-yellow-500" >
-        <h1 className="text-center text-black font-serif">-{book.title}-</h1>
-        <button className='absolute top-2 right-2' onClick={() => onUpdate(book._id)}>
-          <Image
-            src={"/updateIcon.png"}
-            alt={"update book"}
-            width={25}
-            height={25}
-            className="cursor-pointer"
-           />
-        </button>
+        <div className="flex justify-between items-center w-full mb-4">
+          <h1 className="text-black font-serif">-{book.title}-</h1>
+          <button className='flex-shrink-0' onClick={() => onUpdate(book._id)}>
+            <Image
+              src={"/updateIcon.png"}
+              alt={"update book"}
+              width={25}
+              height={25}
+              className="cursor-pointer"
+             />
+          </button>
+        </div>
         <Image
           src={book.imageUrl}
           alt={book.title}
