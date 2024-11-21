00054cfa387eda546f285a0f993de1f2717a7f10
Ryan Fleming <rwf88570@uga.edu>
2024-11-21 18:03:39 -0500

    minor fixes to middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,7 @@ import NextAuth from "next-auth";
 const { auth } = NextAuth(authConfig);
 
 export async function middleware(request: any) {
-    const { nextUrl } = request();
+    const { nextUrl } = request;
     const session = await auth();
     const isAuthenticated = !!session?.user;
     console.log(isAuthenticated, nextUrl.pathname);
@@ -19,7 +19,8 @@ export async function middleware(request: any) {
 
 export const config = {
     matcher: [
-        "/create-item/",
-        "/edit-item/:path*"
+        "/library",
+        "/add-item",
+        "/update-book/:path*"
     ]
 };
