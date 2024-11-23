bf078ae921ad18d150e4cc0f22c38ba5bf384a5d
Ryan Fleming <rwf88570@uga.edu>
2024-11-23 12:04:59 -0500

    Added button prop to Header component

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,30 +1,41 @@
 import Image from "next/image";
-import Link from "next/link";
-
-type HeaderData = {
-  firstLink: string;
-  firstLinkName: string;
-  secondLink: string;
-  secondLinkName: string;
-}
+import Button from "./Button";
 
 interface HeaderProps {
-    header: HeaderData;
+  buttons: {
+    label: string;
+    onClick: (event: React.FormEvent) => void;
+  }[];
 }
 
-export default function Header({header}:HeaderProps) {
-    return(
-       <div>
-        <nav className="flex justify-around space-x-4 items-center bg-blue-500 h-24 p-5 border-b-4" >
-            {/*Group together image and heading to make "Logo"*/}
-            <div className="flex items-center space-x-2">
-                <h1 className="font-serif font-bold text-3xl "> Librium </h1>
-                <Image className="" src="/bookIcon.png" alt= "bookIcon" width={40} height={40} />
-            </div>
-            <Link className="text-lg font-bold underline" href={header.firstLink}>{header.firstLinkName}</Link>
-            <Link className="text-lg font-bold underline" href={header.secondLink}>{header.secondLinkName}</Link>
-
-        </nav>
-       </div> 
-    );
+export default function Header({ buttons }: HeaderProps) {
+  return (
+    <div>
+      <nav className="flex justify-around space-x-4 items-center bg-blue-500 h-24 p-5 border-b-4">
+        
+        <div className="flex items-center space-x-2">
+          <h1 className="font-serif font-bold text-3xl">Librium</h1>
+          <Image
+            className=""
+            src="/bookIcon.png"
+            alt="bookIcon"
+            width={40}
+            height={40}
+          />
+        </div>
+        
+        <div className="flex space-x-4">
+          {buttons.map((button, index) => (
+            <Button
+              key={index}
+              type="button"
+              onClick={button.onClick}
+            >
+              {button.label}
+            </Button>
+          ))}
+        </div>
+      </nav>
+    </div>
+  );
 }
