61968e92aca729431e0f5f2fa632e67c7314ecbb
Ryan Fleming <rwf88570@uga.edu>
2024-12-02 14:49:00 -0500

    Added file upload to add-item, updated books api

--- src/app/add-item/page.tsx
+++ src/app/add-item/page.tsx
@@ -4,14 +4,19 @@ import Button from "../components/Button";
 import React, { useState } from 'react';
 import { useRouter } from "next/navigation";
 import { doLogout } from "..";
+import { useRef } from "react";
+import { useSession } from "next-auth/react"
 
 export default function Additem() {
+    const { data: session, status } = useSession();
     const [book, setBook] = useState({
         title:'',
         imageUrl:'',
     });
     const router = useRouter();
     const [error, setError] = useState("");
+    const file = useRef<HTMLInputElement>(null);
+    
 
     const addItemHeaderButtons = [
       {
@@ -30,18 +35,28 @@ export default function Additem() {
     const onSubmit = async (event: React.FormEvent) => {
         event.preventDefault();
         
-        if (!book.title || !book.imageUrl) {
+        const owner = session?.user?.username;
+        if (!book.title || !book.imageUrl || !owner || !file) {
+          console.log(file);
+          console.log("User: "+owner);
+          console.log("public/uploads/" + file?.current?.files?.[0].name!)
           setError("Cannot add book. Please try again");
           return;
-      }
+        }
+
+        const bookForm = new FormData();
+        bookForm.append("title", book.title);
+        bookForm.append("imageUrl", book.imageUrl);
+        bookForm.append("file", file?.current?.files?.[0]!);
+        bookForm.append("owner", owner);
 
         try {
           const response = await fetch('api/books', {
             method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(book),
+            //headers: {
+            //    'Content-Type': 'application/json'
+            //},
+            body: bookForm,
           });
 
           if (!response.ok) {
@@ -75,7 +90,9 @@ export default function Additem() {
                 placeholder="Enter a link to a cover."
                 value={book.imageUrl}
                 onChange={(e) => setBook({...book, imageUrl: e.target.value})}/>
-
+                <h1 className="font-bold text-lg">Upload file:</h1>
+                <input type="file" id="file-path" ref={file} className="w-full p-2 mt-2 border text-blue-600 border-blue-300 rounded-md"
+                placeholder="Select an epub file."/>
             {error && (
               <p className="text-red-600 font-bold">{error}</p>
             )}
--- src/app/api/books/route.ts
+++ src/app/api/books/route.ts
@@ -1,4 +1,5 @@
 import connectMongoDB from "@/libs/mongodb";
+import { uploadFile } from "@/libs/uploadFile";
 import Item from "@/models/bookSchema";
 import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
@@ -10,10 +11,24 @@ export async function GET(request: NextRequest) {
 }
 
 export async function POST(request: NextRequest) {
-  const { title, imageUrl } = await request.json();
-  await connectMongoDB();
-  await Item.create({title, imageUrl});
-  return NextResponse.json({message: "Book added successfully"}, {status: 201})
+  try {
+
+    const formData = await request.formData();
+
+    const title = formData.get("title");
+    const imageUrl = formData.get("imageUrl");
+    const file = formData.get("file");
+    const owner = formData.get("owner");
+    const filePath = "public/uploads/" + file?.current?.files?.[0].name;
+    
+    await uploadFile(await request.formData());
+    await connectMongoDB();
+    await Item.create({title, imageUrl, filePath, owner});
+    return NextResponse.json({message: "Book added successfully"}, {status: 201})
+  } catch (err: any) {
+    console.log(`Error: ${err.message}`);
+    return NextResponse.json({status: "fail", error: err})
+  }
 }
 
 
