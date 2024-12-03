d4946dd5197dfbe50ab1702b3522adb56e7634fd
Ryan Fleming <rwf88570@uga.edu>
2024-12-02 20:13:03 -0500

    Added landing page button to Header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Button from "./Button";
-
+import { useRouter } from "next/navigation";
 interface HeaderProps {
   buttons: {
     label: string;
@@ -9,11 +9,15 @@ interface HeaderProps {
 }
 
 export default function Header({ buttons }: HeaderProps) {
+  const router = useRouter();
+  const onClick = () => {
+    router.push('/');
+  }
   return (
     <div className="relative z-20">
       <nav className="flex justify-around space-x-4 items-center bg-blue-500 h-24 p-5 border-b-4 shadow-lg">
         <div className="flex items-center space-x-2">
-          <h1 className="font-serif font-bold text-3xl">Librium</h1>
+          <Button type="button" onClick={onClick}> <h1 className="font-serif font-bold text-3xl">Librium</h1></Button>
           <Image
             className=""
             src="/bookIcon.png"
