8536be69c225d18766f5c67eb774b6bd51062db2
ajv20620 <ajv20620@uga.edu>
2024-12-02 19:28:55 -0500

    Update HomePage.tsx

--- src/app/components/HomePage.tsx
+++ src/app/components/HomePage.tsx
@@ -7,7 +7,6 @@ import { useRouter } from "next/navigation";
 export default function HomePage({ session }: { session: any }) {
   const router = useRouter();
 
-  // Buttons for logged-in and logged-out users
   const loggedInHeaderButtons = [
     {
       label: `Hello ${session?.user?.username || "User"}! Click here to go to your library.`,
